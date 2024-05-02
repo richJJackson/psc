9c3c825811ade709c56849c908f698ffacf86880
richJJackson <richj23@liverpool.ac.uk>
2024-05-02 16:37:21 +0100

    amended example

--- Example/psc_example_err.R
+++ Example/psc_example_err.R
@@ -18,25 +18,81 @@ data <- read.csv("exData.csv")
 ### Getting model
 load("model.R")
 
+
+### Example of PSC in R
+
+remove.packages("psc")
+library(devtools)
+install_github("RichJJackson/psc",force=T)
+library(psc)
+library(survival)
+library(mvtnorm)
+
+## loading data and model
+setwd("Example")
+
+### Getting data
+load("exData.R")
+
+### Getting model
+load("model.R")
+
 #### Survival Model
 
+
 ### Running basic
-psc <- psc(model,data)
+res <- pscfit(model,data)
+
+
+
+
+## Example output
+plot(res)
+summary(res)
+
+## Adding
+s.ob <- Surv(data$time,data$cen)
+sfit <- survfit(s.ob~1)
+lines(sfit,col="forestgreen",lwd=3)
+
+
+
+# Individaul treatment effects (takes a while)
+
+ite <- psc_ite(model,data)
+ite <- ite[order(ite[,1]),]
+
+plot(density(ite[,1]))
+
+plot(ite[,1],dum,xlim=c(-3,3))
+segments(ite[,2],dum,ite[,3],dum)
+
+psc_all <- pscfit(model,data)
+
+coef(psc_all)
+abline(v=c(0.1,0.57,0.36))
+median(ite[,1])
+
+
+
+
 
 
 ### Running with missing covariate
 data_err_1 <- data[,-3]
-psc <- psc(fpm,data_err_1)
+res <- psc(fpm,data_err_1)
 
 ### Running with missing data (should give warning)
 data_err_2 <- data
 data_err_2[2:5,2:5] <- NA
-psc <- psc(fpm,data_err_2)
+res <- psc(fpm,data_err_2)
 
 ### Running with misnamed outcome
 data_err_3 <- data
 names(data_err_3)[which(names(data_err_3)=="time")] <- "surv_time"
-psc <- psc(fpm,data_err_3)
+res <- psc(fpm,data_err_3)
+
+
 
 
 
@@ -45,7 +101,7 @@ psc <- psc(fpm,data_err_3)
 # Warning on Simulation size
 # Warning on data distribution
   # Continuous outside of range
-  # Categ
+  # Categorical not included
 
 
 
