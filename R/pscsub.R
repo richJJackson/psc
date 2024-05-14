0845788f67953579a5e2eb089f3e3c26915c3c7e
richJJackson <richj23@liverpool.ac.uk>
2024-05-14 11:32:22 +0100

    Added functions for sub-group analysis

---
+++ R/pscsub.R
@@ -0,0 +1,12 @@
+
+pscsub <- function(CFM,DC,sg,cont_lev=2,cont_cut=NULL){
+
+  pscsub <- pscfitsub(CFM,DC,sg,cont_lev=2,cont_cut=NULL)
+  pscsubWrap(pscsub)
+
+}
+
+
+
+
+
---
+++ R/pscsubWrap.R
@@ -0,0 +1,13 @@
+pscsubWrap <- function(pscsub){
+  tb <- pscsub$Table
+  n <- rowSums(tb)
+  e <- tb[,2]
+  co <- lapply(pscsub$fit,coef.psc)
+  co <- t(matrix(unlist(co),ncol=nrow(tb)))
+
+  hr <- paste(round(exp(co[,1]),2)," (",round(exp(co[,2]),2),", ",round(exp(co[,3]),2),")",sep="")
+
+  ret <- as.matrix(cbind(n,e,co,hr))
+  colnames(ret) <- c("n","e","median","lower","upper","P(x<0)","hr")
+  return(data.frame(ret))
+}
