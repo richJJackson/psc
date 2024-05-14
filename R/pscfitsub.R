d427d39c521a5d848f21fd945b0e64082ab874f5
richJJackson <richj23@liverpool.ac.uk>
2024-05-14 12:10:59 +0100

    bug in pscsubfit

--- R/pscfitsub.R
+++ R/pscfitsub.R
@@ -1,24 +1,24 @@
 
 pscfitsub <- function(CFM,DC,sg,cont_lev=2,cont_cut=NULL){
 
-
   if(class(sg)%in%c("character","factor")){
     sg.id <- factor(sg)
     lev <- levels(sg)
   }
 
-  if(class(sg)%in%c("numeric","integer")){
-    if(is.null(cont_cut)){
-      sg.id <- cut(order(sg),breaks=cont_lev)
+  if (class(sg) %in% c("numeric", "integer")) {
+    if (is.null(cont_cut)) {
+      sg.id <- cut(sg, breaks = cont_lev)
     }
-    if(!is.null(cont_cut)){
-      sg.id <- cut(sg,c(-Inf,cont_cut,Inf))
+    if (!is.null(cont_cut)) {
+      sg.id <- cut(sg, c(-Inf, cont_cut, Inf))
     }
     lev <- levels(sg.id)
   }
 
   nlev <- length(lev)
-  tb <- table(sg.id,dc$cen);tb
+  tb <- table(sg.id, DC$cen)
+
 
   fit <- list()
 
