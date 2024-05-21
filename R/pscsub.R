230085d7ce82a33a4a3fb4b6c0156e35945f589f
richJJackson <richj23@liverpool.ac.uk>
2024-05-21 10:56:39 +0100

    pscsub update

--- R/pscsub.R
+++ R/pscsub.R
@@ -10,10 +10,10 @@
 pscsub <- function(CFM,DC,sg,cont_lev=2,cont_cut=NULL){
 
   pscsub <- pscfitsub(CFM,DC,sg,cont_lev=2,cont_cut=NULL)
-  pscsubWrap(pscsub)
+  res <- pscsubWrap(pscsub)
+  list("pscsub"=pscsub,"table"=res)
 
 }
-
 
 
 
