4a9d94a8e3fe05a713a654543681aa9b1e1bbf62
richJJackson <165043314+richJJackson@users.noreply.github.com>
2024-10-07 15:43:06 +0100

    bug fixes

--- Example/psc_develop.R
+++ Example/psc_develop.R
@@ -23,6 +23,48 @@ coef(surv.psc)
 summary(surv.psc)
 plot(surv.psc)
 
+data[1:3,]
+
+pc.mtc <- pscfit(surv.mod,data,trt=data$trt)
+
+
+
+DC_clean <- pc.mtc$DC_clean
+summary(pc.mtc)
+
+pscSumm <- function (DC_clean)
+{
+
+  lp <- linPred(DC_clean, resp = F)
+  resp <- linPred(DC_clean, resp = T)
+  if ("glm" %in% DC_clean$model.type) {
+    exp_resp <- mean(resp)
+    ob_resp <- mean(unlist(DC_clean$out))
+  }
+  if ("flexsurvreg" %in% DC_clean$model.type) {
+    s.ob <- Surv(DC_clean$out$time, DC_clean$out$cen)
+    sfit <- survfit(s.ob ~ 1)
+    sfit.tab <- summary(sfit)$table
+    exp_resp <- median(resp)
+    ob_resp <- sfit.tab[7]
+  }
+  cat(paste(nrow(DC_clean$cov), "observations selected from the data cohort for comparison"),
+      "\n")
+  cat("CFM of type", DC_clean$model.type, "identified", " \n")
+  cat(paste("linear predictor succesfully obtained with a median of ",
+            round(median(lp), 3)), "\n")
+  cat(paste("Average expected response:", round(exp_resp, 3)),
+      "\n")
+  cat(paste("Average observed response:", round(ob_resp, 3)),
+      "\n")
+  ret <- list(linpred = lp, `E(patResp)` = resp, expResp = exp_resp,
+              obResp = ob_resp)
+  return(ret)
+}
+
+
+
+
 
 
 data("bin.mod")
